39b3a9ac2f4c010a50b9aaf54f2502f6b4ff006e
ChaninEom <eci0623@naver.com>
2022-03-05 23:48:07 +0900

    python greedy 2138 commit(silver1)_fail

--- python/greedy/2138.py
+++ python/greedy/2138.py
@@ -1,3 +1,4 @@
+# mycode
 from posixpath import join
 import sys
 
@@ -39,3 +40,39 @@ else:
         print(calc)
     else:
         print(-1)
+
+
+# top tear code
+# n = int(input())
+# data = list(str(input()))
+# d = data[:]
+# t = list(str(input()))
+# ans = 0
+
+# for i in range(1, n):
+#     if d[i-1] != t[i-1]:
+#         d[i-1] = '1' if d[i-1] == '0' else '0'
+#         d[i] = '1' if d[i] == '0' else '0'
+#         if i + 1 < n:
+#             d[i+1] = '1' if d[i+1] == '0' else '0'
+#         ans += 1
+
+# if ''.join(d) == ''.join(t):
+#     print(ans)
+# else:
+#     d = data
+#     ans = 1
+#     d[0] = '1' if d[0] == '0' else '0'
+#     d[1] = '1' if d[1] == '0' else '0'
+
+#     for i in range(1, n):
+#         if d[i-1] != t[i-1]:
+#             d[i-1] = '1' if d[i-1] == '0' else '0'
+#             d[i] = '1' if d[i] == '0' else '0'
+#             if i + 1 < n:
+#                 d[i+1] = '1' if d[i+1] == '0' else '0'
+#             ans += 1
+#     if ''.join(d) == ''.join(t):
+#         print(ans)
+#     else:
+#         print(-1)
